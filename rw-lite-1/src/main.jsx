6c6ca987a7d99a4bfde9666a87af98e0de393a80
LarsG <lars.ga.solheim@gmail.com>
2023-02-22 16:43:28 +0000

    setup React for rendering

---
+++ rw-lite-1/src/main.jsx
@@ -0,0 +1,9 @@
+// import the other bits
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+// Initializing React here
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <h1>Hello World</h1>
+)
