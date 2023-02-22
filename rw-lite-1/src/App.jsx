2db64d029755148208a718e4e1450b848f6c6dc1
LarsG <lars.ga.solheim@gmail.com>
2023-02-22 16:49:35 +0000

    set basic styling

--- rw-lite-1/src/App.jsx
+++ rw-lite-1/src/App.jsx
@@ -1,5 +1,21 @@
+// CSS
+import './styles/index.css'
+
+// Here we are going to write the whole of our application
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="App">
+      <header className="header">
+        <h2>Header</h2>
+      </header>
+
+      <main className="main">
+        <h1>Content</h1>
+      </main>
+
+      <footer className="footer">
+        <h3>Footer</h3>
+      </footer>
+    </div>
   )
 }
